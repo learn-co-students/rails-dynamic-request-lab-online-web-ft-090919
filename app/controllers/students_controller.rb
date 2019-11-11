0c6b1fa7593adb01fbf577fc4b1582fb01d94aef
Richell Bonfield <richell.bonfield12@gmail.com>
2019-11-11 01:35:58 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,11 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+  
+  def show
+  end
+
+  def set_student
+      @student = Student.find(params[:id])
+  end
 end
